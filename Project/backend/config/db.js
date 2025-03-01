6c6279bc542b31f735841c0731ae73aee0616d9d
rutmanTanya <t.rutman13@gmail.com>
2025-03-01 18:51:01 +0200

    Update db.js

--- Project/backend/config/db.js
+++ Project/backend/config/db.js
@@ -1,21 +1,37 @@
 //Database connection files
 
 const mysql = require("mysql2");
-const config = require("./config"); 
+const config = require("./config");
 
-const db = mysql.createConnection({
-    host: config.DB_HOST,
-    user: config.DB_USER,
-    password: config.DB_PASSWORD,
-    database: config.DB_NAME
-});
+let connection;
+const db = {
+    getConnection: () => {
+        if (!connection) {
+            connection = mysql.createConnection({
+                host: config.DB_HOST,
+                user: config.DB_USER,
+                password: config.DB_PASSWORD,
+                database: config.DB_NAME
+            });
 
-db.connect(err => {
-    if (err) {
-        console.error("Database connection failed:", err);
-        return;
-    }
-    console.log("Connected to MySQL Database");
-});
+            connection.connect((err) => {
+                if (err) {
+                    console.error("Database connection failed:", err);
+                    throw err;
+                }
+                console.log("Connected to MySQL Database");
+            });
+
+            //Disconnection errors
+            connection.on("error", (err) => {
+                console.error("Database connection error:", err);
+                if (err.code === "PROTOCOL_CONNECTION_LOST") {
+                    connection = null; // Reset connection
+                }
+            });
+        }
+        return connection;
+    },
+};
 
 module.exports = db;
