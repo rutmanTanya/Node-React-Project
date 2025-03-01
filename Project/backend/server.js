8c72b0d5d266ce89b4ca5d77510473a3d308c592
rutmanTanya <t.rutman13@gmail.com>
2025-03-01 18:50:35 +0200

    Update server.js

--- Project/backend/server.js
+++ Project/backend/server.js
@@ -1,12 +1,11 @@
 //Project submitted by: Tanya Rotman, Alex Lapin. Class 48/6
-
 const express = require('express'); 
 const session = require('express-session'); 
 const path = require("path");
 const config = require("./config/config.js");
 const db = require("./config/db");
 
-//Authentication middleware
+// Authentication middleware
 const authMiddleware = require("./middleware/authMiddleware");
 
 const app = express();
@@ -14,37 +13,28 @@ const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 
-// Serve frontend from backend
+// Serve frontend from backend 
 app.use(express.static(path.join(__dirname, "../build")));
 
 // Session setup
 app.use(session({
     secret: config.SESSION_SECRET,
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
+    cookie: { secure: false }
 }));
 
-db.connect(err => {
-    if (err) {
-        console.error("Database connection failed:", err);
-    } else {
-        console.log("Connected to MySQL Database");
-    }
-});
+//DB connection
+db.getConnection();
 
 // Route to check if the server is running
 app.get("/", (req, res) => {
     res.send("Schedule & Logistics Backend Running");
 });
 
-//Route to check if the frontend is connected to backend
+// API route to check connection
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from Backend!" });
-});
-
-// All other routes → Serve React app
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
 // Routes
@@ -54,7 +44,12 @@ const workerRoutes = require("./routes/workerRoutes");
 app.use("/api/auth", authRoutes);
 app.use("/api/workers", authMiddleware([1]), workerRoutes);
 
+//React frontend connection
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
+
 // Start the server
 app.listen(PORT, () => {
-    console.log("Server running on http://localhost:4000");
+    console.log(`Server running on http://localhost:${PORT}`);
 });
